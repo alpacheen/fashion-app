c8bfa6e3febcf9097e8f88f09c84c74420d740db
alpacheen <a.muir.1987@hotmail.com>
2025-04-25 11:25:19 +1000

    update footer logo

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -12,11 +12,22 @@ const Footer = () => {
   return (
     <FooterSection>
       <LogoContainer>
-        <img
+        <svg
+          className="w-[6rem] h-auto overflow-visible"
+          style={{ strokeLinecap: "round", strokeLinejoin: "round" }}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <g stroke="#faebd7">
+            <path d="M12,17.27L18.18,21l-1.64-7.03L22,9.24l-7.19-0.61L12,2L9.19,8.63L2,9.24l5.46,4.73L5.82,21L12,17.27z" />
+          </g>
+        </svg>
+        {/* <img
           src={Logo}
           alt="Logo"
           className="w-20 h-auto overflow-visible fill-none"
-        />
+        /> */}
         <h3 className='text-3xl font-["Sirin"]'>Vibe Studios</h3>
       </LogoContainer>
       <FooterComponent
